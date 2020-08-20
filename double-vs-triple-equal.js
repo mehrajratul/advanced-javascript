7cd436ece67bba74856afe69093080a61b6aae93
mehrajratul <67520670+mehrajratul@users.noreply.github.com>
2020-08-20 19:07:31 +0600

    hi

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+const first = 2;
+const second = "2";
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+//"==" - doesn't checks the type; 
+//"===" - strictly checks the type;
+// to compare use ===;
